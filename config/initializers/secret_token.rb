b127ec85e008db561c9ef991ec82f876e3ae0918
nwatkins <moniker.surname@gmail.com>
2013-03-02 11:25:52 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pinwheels::Application.config.secret_token = 'c107cdd6da8635315ee8e17554b1cd51793edec7befa9d530cfb78b92901883f0dc4a20dab8d0b7263ef5c312423771493b76d43b2d5622d884d97ccb120ed16'
