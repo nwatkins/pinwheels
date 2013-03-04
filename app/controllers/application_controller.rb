56789f1194d4d11babb0d3c49508848dd452c81a
nwatkins <moniker.surname@gmail.com>
2013-03-03 21:54:28 -0500

    New image assets Canvas resizes correctly Added a bunch of styling Fixed issue with skill node rendering Code cleanup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,28 @@ class ApplicationController < ActionController::Base
     protect_from_forgery
 
     def index
+        # Create pinwheels object
+        $pinwheels = {}
+        pinwheels = Pinwheel.joins{ skills }.includes{ skills }.order{ name }
+        pinwheels.each do |pinwheel|
+            $pinwheels[pinwheel.name.parameterize('_')] = {
+                :unid => pinwheel.id,
+                :name => pinwheel.name,
+                :skills => pinwheel.skills.collect{ |s| s.name.parameterize('_') }
+            }
+        end
+        
+        # Create skills object
+        $skills = {}
+        skills = Skill.joins{ |s| s.pinwheels }.includes{ |s| s.pinwheels }.order{ name }
+        skills.each do |skill|
+            $skills[skill.name.parameterize('_')] = {
+                :unid => skill.id,
+                :name => skill.name,
+                :pinwheels => skill.pinwheels.collect{ |p| p.name.parameterize('_') }
+            }
+        end
+
         render :template => 'index'
     end
 end
